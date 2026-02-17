7aba78ae4049da2d1a9fa12b0b4ab7cb7443de32
Jindřich Ochmann <jindrich.ochmann@gmail.com>
2026-02-17 03:11:55 +0100

    i18n

---
+++ src/content/config/i18n/iso_4217.ts
@@ -0,0 +1,10 @@
+// currencies
+export const iso_4217: Record<string, { name: string }> = {
+  USD: { name: "US Dollar" },
+  EUR: { name: "Euro" },
+  CZK: { name: "Czech Koruna" },
+  GBP: { name: "British Pound" },
+  ZMW: { name: "Zambian Kwacha" },
+  ZAR: { name: "South African Rand" },
+  // ... 
+};
